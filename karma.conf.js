4620dd8466d11b1244ef636f3019219b23cf2eb5
Alexis Hope <alexis@cvofhope.com>
2017-08-23 10:10:51 +1000

    update karma conf to include build file over src file

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/*.js',
+      'build/route.js',
       'tests/*Spec.js'
     ],
 
